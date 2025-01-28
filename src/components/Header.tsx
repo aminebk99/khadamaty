0924db7df4fa5aa07d2033e78967ed0bf63f9bab
Mohamed Amine Bazaou <129026688+aminebk99@users.noreply.github.com>
2025-01-28 22:54:59 +0100

    commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom";
+import logo from '../assets/khadamatylogopng.png'
 
 function Header() {
   return (
     <header className="text-white w-full flex justify-between items-center p-4 bg-green-800">
-      <div>Logo</div>
+      <div><img src={logo} alt="" /></div>
       <div>
         <Link to="/signup" className=" mr-4">S'inscrire</Link>
         <Link to="/login" className="ml-4">Connexion</Link>
