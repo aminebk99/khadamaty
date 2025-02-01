c777d37acbb728c2fe1b361caa2314301e335e2c
Mohamed Amine Bazaou <129026688+aminebk99@users.noreply.github.com>
2025-02-01 18:52:47 +0100

    commit login page

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,11 +1,20 @@
 import Header from "../components/Header";
 import LoginForm from "../components/LoginForm";
+import logo from "../assets/khadamatylogopng.png"
 
 function LoginPage() {
   return (
     <div>
       <Header />
-      <LoginForm />
+      <div className="grid grid-cols-1 xl:grid-cols-2 h-screen">
+        {/* Hide the logo on medium screens and show it only on xl screens */}
+        <div className="hidden xl:flex bg-[#146551] items-center justify-center">
+          <img src={logo} alt={logo} />
+        </div>
+        <div className=" flex items-center justify-center">
+          <LoginForm />
+        </div>
+      </div>
     </div>
   );
 }
